270556ee16061e5259b9747bba0f2110d11e6c7f
diego9677 <dieguito9677@gmail.com>
2023-12-22 17:03:55 -0400

    porfolio init

--- web_app/src/App.tsx
+++ web_app/src/App.tsx
@@ -8,10 +8,46 @@ function App() {
   // }, [])
 
   return (
-    <main className="bg-neutral-800" style={{ height: `${window.innerHeight}px` }}>
-      app
+    <main className="bg-slate-800" style={{ height: `${window.innerHeight}px` }}>
+
+      <section className="w-full md:w-[700px] mx-auto">
+        <header className="h-20 flex items-center justify-end">
+          <button>
+            <i className="las la-sun la-lg text-white" />
+          </button>
+        </header>
+
+        {/* personal info  */}
+        <div className="flex gap-7 border-b border-dashed pb-16">
+          <div className="w-44 h-44 border rounded-full shadow-lg shadow-slate-900">
+          </div>
+
+          <section className="flex flex-col gap-4">
+            <h1 className="text-4xl font-bold text-neutral-100">Diego AP</h1>
+            <p className="text-xl font-normal text-neutral-300">frase apacionada en una linea</p>
+            {/* social section */}
+            <div className="flex gap-2"></div>
+          </section>
+
+        </div>
+
+        {/* experience */}
+
+        <section className="flex flex-col pt-14 gap-10">
+          <h3 className="text-2xl font-bold text-neutral-100">Experiencia</h3>
+          <div>
+
+          </div>
+        </section>
+
+        {/* proyectos */}
+        <section className="flex flex-col gap-10 pt-10">
+          <h3 className="text-2xl font-bold text-neutral-100">Proyectos</h3>
+        </section>
+
+      </section>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
